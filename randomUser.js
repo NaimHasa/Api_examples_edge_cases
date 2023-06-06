b352d953ec05e7e51c78ef07d8dc641d65d07dec
Naim Hasan <naimarafat7@gmail.com>
2023-06-06 00:29:18 -0700

    nested_api

---
+++ randomUser.js
@@ -0,0 +1,27 @@
+const randomUserDataLoad = () => {
+    fetch('https://randomuser.me/api/?results=10')
+        .then(res => res.json())
+        .then(data => displayLoadData(data.results));
+}
+
+const displayLoadData = users => {
+
+    const userContainer = document.getElementById('randomUsers');
+    for (const user of users) {
+        console.log(user);
+        const userDiv = document.createElement('div')
+        userDiv.innerHTML = `
+        
+        <h1>Name: ${user.name.first}</h1>
+        <h4>Email: ${user.email}</h4>
+        <p>Location: ${user.location.city}<p>
+        
+        `
+        userContainer.appendChild(userDiv);
+
+    }
+
+}
+
+
+randomUserDataLoad();
